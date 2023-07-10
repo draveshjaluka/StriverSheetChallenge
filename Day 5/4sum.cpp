59e04c60038a6ba2bcd543c7d2f17d16d6b64d9c
Dravesh Jaluka <77679322+draveshjaluka@users.noreply.github.com>
2023-07-11 03:18:39 +0530

    Create 4sum.cpp

---
+++ Day 5/4sum.cpp
@@ -0,0 +1,39 @@
+/* Question Link:
+https://leetcode.com/problems/4sum/
+https://www.codingninjas.com/codestudio/problems/4sum_5713771 */
+
+/* Solution: extended version of 3sum, added one more outer loop the 3 sum problem and solved */
+
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i< nums.size(); i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j< nums.size(); j++){
+                if(j> i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = nums.size() - 1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        vector <int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        while(k<l && nums[k] == nums[k+1]) k++;
+                        while(k<l && nums[l] == nums[l-1]) l--;
+                        k++;
+                        l--;
+                    }
+                    else if(sum > target) l--;
+                    else k++;
+                }
+            }
+        }
+        return ans;
+    }
+};
