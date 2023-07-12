be777442a05102f17190338ce9fd3e5e0d3ec926
Dravesh Jaluka <77679322+draveshjaluka@users.noreply.github.com>
2023-07-13 03:47:09 +0530

    Update RotataLinkedList.cpp

--- Day 7/RotataLinkedList.cpp
+++ Day 7/RotataLinkedList.cpp
@@ -2,5 +2,36 @@
 https://bit.ly/33bNQPo
 https://leetcode.com/problems/rotate-list/description/ */
 
-/* Solution: Simply used a two pointer approach to reach the two different heads of linked list and then joined them but changing nullptr position */
+/* Solution: Simply used a two-pointer approach to reach the two different heads of the linked list and then joined them but changing nullptr position */
 
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        int s=0;
+        ListNode *temp=head;
+        if(head==NULL || head->next==NULL || k==0) return head;
+        while(temp!=NULL)
+        {
+            s++;
+            temp=temp->next;
+        }
+        temp=head;
+        k=k%s;
+        if(k)
+        {while(s-k>1)
+        {
+            temp=temp->next;
+            s--;
+        }
+        ListNode *nhead=temp->next;
+        temp->next=NULL;
+        temp=nhead;
+        while(temp->next !=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=head;
+        head=nhead;}
+        return head;
+    }
+};
