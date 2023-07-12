849f619f3c958280c72928dcd6d0ee2988fbfbc3
Dravesh Jaluka <77679322+draveshjaluka@users.noreply.github.com>
2023-07-13 03:36:45 +0530

    Create RotataLinkedList.cpp

---
+++ Day 7/RotataLinkedList.cpp
@@ -0,0 +1,6 @@
+/* Question Link: 
+https://bit.ly/33bNQPo
+https://leetcode.com/problems/rotate-list/description/ */
+
+/* Solution: Simply used a two pointer approach to reach the two different heads of linked list and then joined them but changing nullptr position */
+
