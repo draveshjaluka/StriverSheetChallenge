597fb464ae0abd478f84afb2577d0b5f0e90a58b
Dravesh Jaluka <77679322+draveshjaluka@users.noreply.github.com>
2023-07-15 01:47:13 +0530

    Create Nmeetings.cpp

---
+++ Day 9/Nmeetings.cpp
@@ -0,0 +1,41 @@
+/* Question Link:
+https://www.codingninjas.com/studio/problems/maximum-meetings_1062658 */
+
+/* Solution: Stores all types of data in a structute and then sorted according to end time and got the solution */
+struct meeting{
+    int st;
+    int en;
+    int po;
+};
+bool static cmp(struct meeting m1,struct meeting m2)
+{
+    if(m1.en<m2.en) return true;
+    else if(m1.en>m2.en) return false;
+    else if(m1.po<m2.po) return true;
+    return false;
+}
+int maximumMeetings(vector<int> &s, vector<int> &e)
+{
+    // Write your code here
+    int n=e.size();
+    struct meeting m[n];
+    for(int i=0;i<n;i++)
+    {
+        m[i].st=s[i];m[i].en=e[i];m[i].po=i+1;
+    }
+
+    sort(m,m+n,cmp);
+
+    int ans=0;
+
+    int last=m[0].en;
+    for(int i=1;i<n;i++)
+    {
+        if(m[i].st>last)
+        {
+            last=m[i].en;
+            ans++;
+        }
+    }
+    return ans+1;
+}
